3a1018224c4f0d81851c9ddc6b730d21ea1889da
Trung <trungvd@compedia.vn>
2022-08-09 12:24:45 +0700

    delinquent commit

--- src/app/pages/delinquent-manager/delinquent-manager-list/delinquent-manager-list.component.ts
+++ src/app/pages/delinquent-manager/delinquent-manager-list/delinquent-manager-list.component.ts
@@ -7,9 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DelinquentManagerListComponent implements OnInit {
 
+  delinquentManager : delinquentManager[] = [
+    {
+      id: 1,
+      name: "cao hải đăng",
+      fined_amount: "100.000 VND",
+      proceeds: "200.000 VND",
+      staff_id: "abc123"
+    },
+    {
+      id: 1,
+      name: "cao hải đăng",
+      fined_amount: "100.000 VND",
+      proceeds: "200.000 VND",
+      staff_id: "abc123"
+    },
+    {
+      id: 1,
+      name: "cao hải đăng",
+      fined_amount: "100.000 VND",
+      proceeds: "200.000 VND",
+      staff_id: "abc123"
+    },
+    {
+      id: 1,
+      name: "cao hải đăng",
+      fined_amount: "100.000 VND",
+      proceeds: "200.000 VND",
+      staff_id: "abc123"
+    },
+    {
+      id: 1,
+      name: "cao hải đăng",
+      fined_amount: "100.000 VND",
+      proceeds: "200.000 VND",
+      staff_id: "abc123"
+    }
+  ]
+
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+interface delinquentManager  {
+  id: number,
+  name: string,
+  fined_amount: string,
+  proceeds: string,
+  staff_id: string,
 }
