0b0132180173a713f533cf8e46824632baac8ddd
Dang <dangch@compedia.vn>
2022-08-09 14:19:10 +0700

    Merge branch 'dev' of E:\FE\compedia_fe with conflicts.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-
+  production: false,
+  api_url: 'http://192.168.100.63:8080/'
 };
 
 /*
