addb49335e10a4420906d2ac3e8e2b2dc3346212
HoangTH <hoangtran8032@gmail.com>
2022-08-12 11:29:44 +0700

    commit

--- src/app/models/requests/account-manager.request.ts
+++ src/app/models/requests/account-manager.request.ts
@@ -11,13 +11,13 @@ export interface IAccountManagerRequest {
 }
 
 export interface IEditAccountManagerRequest {
-  id: number,
-  username: string,
-  password: string,
-  full_name: string,
-  date_of_birth: string,
-  phone: string,
+  dob: string,
   email: string,
-  status?: string,
-  role_id: string
+  full_name: string,
+  id: number,
+  password: string,
+  phone: string,
+  roleId: number,
+  username: string
+
 }
