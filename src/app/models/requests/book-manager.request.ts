ced1bda8f5d7b966f72ec029b09bf30d66618164
Dang <dangch@compedia.vn>
2022-08-12 11:30:50 +0700

    Đăng commit-author-api

---
+++ src/app/models/requests/book-manager.request.ts
@@ -0,0 +1,12 @@
+export interface IBookManagerRequest {
+  bookId: number,
+  bookName: string,
+  idAuthor: number,
+  publishingYear: string,
+  pageNumber: number,
+  image: string,
+  price: number,
+  idTypeBook: number,
+  idCompany: number,
+  amount: number,
+}
---
+++ src/app/models/responses/book-manager.response.ts
@@ -0,0 +1,12 @@
+export interface IBookManagerResponse {
+  bookId: number,
+  bookName: string,
+  idAuthor: number,
+  publishingYear: string,
+  pageNumber: number,
+  image: string,
+  price: number,
+  idTypeBook: number,
+  idCompany: number,
+  amount: number,
+}
