3e216251eabdc132a04160f0031df27a04e3446a
Dang <dangch@compedia.vn>
2022-08-20 09:16:13 +0700

    user-update

--- src/app/core/pages/user-manager/user-manager.module.ts
+++ src/app/core/pages/user-manager/user-manager.module.ts
@@ -7,6 +7,7 @@ import {MessageService} from "primeng/api";
 
 import { UserManagerListComponent } from './user-manager-list/user-manager-list.component';
 import { UserManagerRoutingModule } from "./user-manager-routing.module";
+import {TableModule} from "primeng/table";
 
 @NgModule({
   declarations:  [
@@ -18,7 +19,7 @@ import { UserManagerRoutingModule } from "./user-manager-routing.module";
     UserManagerRoutingModule,
     ReactiveFormsModule,
     ToastModule,
-
+    TableModule
 
 
   ],
