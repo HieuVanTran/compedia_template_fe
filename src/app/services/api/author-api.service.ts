4c28c8513abb97a9eb42875ab7aaf2f8564125de
Dang <dangch@compedia.vn>
2022-09-05 16:52:11 +0700

    dang-commit

--- src/app/services/api/author-api.service.ts
+++ src/app/services/api/author-api.service.ts
@@ -2,9 +2,10 @@ import {Injectable} from "@angular/core";
 import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {IResponseModel} from "../../models/commons/response.model";
+import {IPageResponseModel, IResponseModel} from "../../models/commons/response.model";
 import {IBookAuthorResponse} from "../../models/responses/book-author.response";
 import {IBookAuthorRequest, IEditBookAuthorRequest} from "../../models/requests/book-author.request";
+import {IBookManagerResponse} from "../../models/responses/book-manager.response";
 
 
 
@@ -22,7 +23,13 @@ export class AuthorApiService{
     const url = `${this.api}/author/author`
     return this.http.get<IResponseModel<IBookAuthorResponse[]>>(url)
   }
-
+  _searchAuthor(request: any): Observable<IResponseModel<IPageResponseModel<IBookAuthorResponse>>> {
+    const url = `${this.api}/author/search?page=${request.page}&size=${request.size}
+    ${request.authorName? '&authorName='+request.authorName : ''}
+    ${request.address? '&address='+request.address : ''}
+     ${request.title? '&title='+request.title : ''}`
+    return this.http.get<IResponseModel<IPageResponseModel<IBookAuthorResponse>>>(url)
+  }
   _createNewAuthor(requestBody: IBookAuthorRequest): Observable<IResponseModel<any>> {
     const url = `${this.api}/author`
     return this.http.post<IResponseModel<any>>(url, requestBody)
