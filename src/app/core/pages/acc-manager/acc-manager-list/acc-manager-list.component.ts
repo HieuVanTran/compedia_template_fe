54700dd08aea34becaa080a247019f5910398279
HoangTH <hoangtran8032@gmail.com>
2022-09-05 15:44:05 +0700

    search account commit

--- src/app/core/pages/acc-manager/acc-manager-list/acc-manager-list.component.ts
+++ src/app/core/pages/acc-manager/acc-manager-list/acc-manager-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AccountApiService} from "../../../../services/api/account-api.service";
-import {IResponseModel} from "../../../../models/commons/response.model";
+import {IPageResponseModel, IResponseModel} from "../../../../models/commons/response.model";
 
 import {IAccountManagerResponse} from "../../../../models/responses/account-manager.response";
 import {IAccountManagerView} from "../../../../models/views/account-manager.view";
@@ -10,6 +10,7 @@ import {IRoleManagerView} from "../../../../models/views/role-manager.view";
 import {MessageService} from "primeng/api";
 import {RoleManagerApiService} from "../../../../services/api/role-manager-api.service";
 import {IRoleManagerResponse} from "../../../../models/responses/role-manager.response";
+import {Constant} from "../../../../util/constant";
 
 @Component({
   selector: 'app-acc-manager-list',
@@ -23,10 +24,16 @@ export class AccManagerListComponent implements OnInit {
   accountInfoForm!: FormGroup;
   accountSelected!: IAccountManagerView;
   listRoleManager: IRoleManagerView[] = [];
+  usernameSearch!: string;
+  fullNameSearch!: string;
+  emailSearch!: string;
+  roleIdSearch!: number;
+  page: number = Constant.PAGE_INIT
+  size: number = Constant.SIZE_INIT
 
 
   constructor(private accountApiService: AccountApiService,
-              private fb:FormBuilder,
+              private fb: FormBuilder,
               private messageService: MessageService,
               private roleManagerApiService: RoleManagerApiService) {
     this.accountInfoForm = fb.group({
@@ -72,33 +79,33 @@ export class AccManagerListComponent implements OnInit {
       full_name: this.accountInfoForm.value.full_name,
       dob: this.accountInfoForm.value.date_of_birth,
       email: this.accountInfoForm.value.email,
-      phone: '0' +this.accountInfoForm.value.phone,
+      phone: '0' + this.accountInfoForm.value.phone,
       role_id: this.accountInfoForm.value.role_id
     };
     this.accountApiService._createNewAccount(createNewAccountRequest).subscribe(
       (res: IResponseModel<any>) => {
-        this.messageService.add({severity:'success', summary:'Thông báo!', detail:'Tạo tài khoản thành công! '});
+        this.messageService.add({severity: 'success', summary: 'Thông báo!', detail: 'Tạo tài khoản thành công! '});
         console.log('Tao tai khoan thanh cong');
         this.getAllAccountManager()
       },
       err => {
         console.log(err)
-        this.messageService.add({severity:'error', summary:'Thông báo!', detail:err.error});
+        this.messageService.add({severity: 'error', summary: 'Thông báo!', detail: err.error});
         console.log('Tao tai khoan that bai')
       }
     )
   }
 
   onDeleteAccount() {
-    if(this.accountSelected) {
+    if (this.accountSelected) {
       this.accountApiService._deleteAccount(this.accountSelected.id).subscribe(
         (res: IResponseModel<any>) => {
-          this.messageService.add({severity:'success', summary:'Thông báo!', detail:'Xóa thành công! '});
+          this.messageService.add({severity: 'success', summary: 'Thông báo!', detail: 'Xóa thành công! '});
           console.log('Xoa tai khoan thanh cong');
           this.getAllAccountManager()
         },
         err => {
-          this.messageService.add({severity:'error', summary:'Thông báo!', detail:'Xóa thất bại! '});
+          this.messageService.add({severity: 'error', summary: 'Thông báo!', detail: 'Xóa thất bại! '});
           console.log('Xoa tai khoan that bai')
         }
       )
@@ -106,18 +113,18 @@ export class AccManagerListComponent implements OnInit {
   }
 
   editAccount(i: IAccountManagerView) {
-      this.accountSelected = i;
-      this.accountInfoForm.patchValue(
-        {
-          username: i.username,
-          full_name: i.full_name,
-          date_of_birth: i.date_of_birth,
-          email: i.email,
-          phone: i.phone,
-          role_id: i.role_id
-        }
-      )
-    }
+    this.accountSelected = i;
+    this.accountInfoForm.patchValue(
+      {
+        username: i.username,
+        full_name: i.full_name,
+        date_of_birth: i.date_of_birth,
+        email: i.email,
+        phone: i.phone,
+        role_id: i.role_id
+      }
+    )
+  }
 
   onEditAccount() {
     const editAccountManagerRequest: IEditAccountManagerRequest = {
@@ -132,12 +139,12 @@ export class AccManagerListComponent implements OnInit {
     };
     this.accountApiService._editAccount(editAccountManagerRequest).subscribe(
       (res: IResponseModel<any>) => {
-        this.messageService.add({severity:'success', summary:'Thông báo!', detail:'Chỉnh sửa thành công! '});
+        this.messageService.add({severity: 'success', summary: 'Thông báo!', detail: 'Chỉnh sửa thành công! '});
         console.log('Thay doi thong tin thanh cong');
         this.getAllAccountManager()
       },
       err => {
-        this.messageService.add({severity:'error', summary:'Thông báo!', detail:'Chỉnh sửa thất bại! '});
+        this.messageService.add({severity: 'error', summary: 'Thông báo!', detail: 'Chỉnh sửa thất bại! '});
         console.log('Thay doi thong tin that bai')
       }
     )
@@ -160,10 +167,44 @@ export class AccManagerListComponent implements OnInit {
             name: roleManagerRes.name
           };
           this.listRoleManager.push(roleManagerView)
-        } )
+        })
       }
     )
   }
 
 
+  onSearch() {
+    const searchRequest = {
+      username: this.usernameSearch,
+      fullName: this.fullNameSearch,
+      email: this.emailSearch,
+      roleId: this.roleIdSearch,
+      page: this.page,
+      size: this.size
+    }
+    console.log(searchRequest)
+    this.accountApiService._searchAccount(searchRequest).subscribe(
+      (res: IResponseModel<IPageResponseModel<IAccountManagerResponse>>) => {
+        console.log(res)
+        this.accManager = [];
+        res.data.results.forEach(accountManagerRes => {
+          const accountManagerView: IAccountManagerView = {
+            id: accountManagerRes.account_id,
+            username: accountManagerRes.username,
+            full_name: accountManagerRes.full_name,
+            date_of_birth: accountManagerRes.date_of_birth,
+            email: accountManagerRes.email,
+            phone: accountManagerRes.phone,
+            role_id: accountManagerRes.code_role
+          };
+          this.accManager.push(accountManagerView)
+        })
+        console.log(this.accManager)
+      }
+    )
+  }
+
+  selectRole() {
+    console.log(this.roleIdSearch)
+  }
 }
