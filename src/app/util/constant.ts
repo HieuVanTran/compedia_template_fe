ec36caba6fc8b166d02d78d15045337f25f471bf
Dang <dangch@compedia.vn>
2022-09-05 13:42:38 +0700

    book-update

--- src/app/util/constant.ts
+++ src/app/util/constant.ts
@@ -1,3 +1,5 @@
 export class Constant {
-  public static LIBRARY_STORAGE_KEY =  '3b354a25d9fa723326fd7e1a6d312515'
+  public static readonly LIBRARY_STORAGE_KEY =  '3b354a25d9fa723326fd7e1a6d312515'
+  public static readonly PAGE_INIT: number = 0
+  public static readonly SIZE_INIT: number = 10
 }
