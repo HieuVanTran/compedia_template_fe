2b8781657309c14c04638df15875bffb7d4e21fa
Dang <dangch@compedia.vn>
2022-09-14 17:36:31 +0700

    dang-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {NotFoundComponent} from "./core/not-found/not-found.component";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {AuthInterceptor} from "./services/intercept/auth.interceptor";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormBuilder} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     BrowserAnimationsModule
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    FormBuilder
   ],
   bootstrap: [AppComponent]
 })
