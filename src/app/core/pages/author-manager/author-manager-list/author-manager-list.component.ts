f3cda2e71051698d7e0a086c05bb23089dc5b6e9
hoangtran8032 <hoangth@compedia.vn>
2022-09-27 11:12:15 +0700

    cm

--- src/app/core/pages/author-manager/author-manager-list/author-manager-list.component.ts
+++ src/app/core/pages/author-manager/author-manager-list/author-manager-list.component.ts
@@ -29,7 +29,8 @@ export class AuthorManagerListComponent implements OnInit {
   loading: boolean = true;
   authorName!: string[];
   addresss!: string[];
-
+  selectedSortField!: string
+  selectedSortOrder!: string
 
   constructor(private AuthorApiService: AuthorApiService,
               private messageService: MessageService,
@@ -71,6 +72,8 @@ export class AuthorManagerListComponent implements OnInit {
       size: this.size,
       address: this.address,
       title: this.title,
+      sortField: this.selectedSortField,
+      sortOrder: this.selectedSortField ? this.selectedSortOrder : undefined
     }
     console.log(searchRequest)
     this.AuthorApiService._searchAuthor(searchRequest)
@@ -173,6 +176,8 @@ export class AuthorManagerListComponent implements OnInit {
     this.address = Constant.NULL_VALUE
     this.title = Constant.NULL_VALUE
     this.first = 0
+    this.selectedSortField = Constant.NULL_VALUE
+    this.selectedSortOrder = Constant.NULL_VALUE
     this.onSearchAuthor()
   }
 
@@ -181,9 +186,9 @@ export class AuthorManagerListComponent implements OnInit {
     this.page = $event.first/$event.rows
     // @ts-ignore
     this.size = $event.rows
+    this.selectedSortOrder = $event.sortOrder == 1? 'ACS' : 'DESC'
     // @ts-ignore
-    // this.selectedSortField = $event.sortField
-    // this.selectedSortOrder = $event.sortOrder == 1? 'ACS' : 'DESC'
+    this.selectedSortField = $event.sortField
     console.log($event)
     this.onSearchAuthor()
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  api_url: 'http://192.168.100.35:8017/api/v1',
+  api_url: 'http://192.168.100.90:8087/api/v1',
   file_url: 'http://localhost:8080/resources'
 };
